0550511bb5a9c57cc97cab0c98c2c45996db2334
richard457 <beastar457@gmail.com>
2017-03-20 08:01:33 +0200

    ..

---
+++ testingDir.js
@@ -0,0 +1,8 @@
+//assumming the final path of folder v is like the bellow
+var dir='name/bn/nnn/v';
+console.log(dir.split('/'));
+dir.split('/').forEach(function(e) {
+    console.log(e);
+}, this);
+
+//git folders ids(online) and save them in sqlite3 db
